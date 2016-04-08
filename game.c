6252b244e2be39edd7a3bcab816c66be4eaa1f3c
SealHigh <martin.renstrom1@gmail.com>
2016-04-08 17:26:41 +0200

    test

--- game.c
+++ game.c
@@ -1,9 +1,8 @@
-#include <SDL.h>
-#include "game/game_window/window.h"
+
 
 int main(int argc, char *argv[]) {
 
-    CreateGameWindow("Hej",100,100,400,100);
+    CreateGameWindow("Hej",200,100,400,100);
 
     return 0;
 }
